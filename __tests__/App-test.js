207939f977a1bbfc2ed015b9e5bc087e1cda6b96
Chris Gradwohl <christophergradwohl@gmail.com>
2020-06-12 07:24:36 -0700

    wow.

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,10 +1,13 @@
-/* eslint-disable no-undef */
+/* eslint-disable */
 
 import 'react-native';
 import React from 'react';
 import renderer from 'react-test-renderer';
 
 import App from '../App';
+
+// wierd fix to resolve 'import a file after the Jest environment has been torn down' error.
+jest.useFakeTimers();
 
 it('renders correctly', () => {
   renderer.create(<App />);
