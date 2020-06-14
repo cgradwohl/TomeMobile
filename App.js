cd54ab47a6b4806778cfd531e91690b0a5369568
Chris Gradwohl <christophergradwohl@gmail.com>
2020-06-13 20:31:37 -0700

    adds icons, deps,  router.

--- App.js
+++ App.js
@@ -4,14 +4,48 @@ import {
   Text,
 } from 'react-native';
 
+import { Header } from 'react-native-elements';
+
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
+
+const LibraryScreen = () => (
+  <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Library Screen</Text>
+  </SafeAreaView>
+);
+
+const TomeScreen = () => (
+  <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Tome Screen</Text>
+  </SafeAreaView>
+);
+
+const QueueScreen = () => (
+  <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Queue Screen</Text>
+  </SafeAreaView>
+);
+
+const Router = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Library" component={LibraryScreen} />
+    <Tab.Screen name="Tome" component={TomeScreen} />
+    <Tab.Screen name="Queue" component={QueueScreen} />
+  </Tab.Navigator>
+);
 
 export default function App() {
   return (
     <NavigationContainer>
-      <SafeAreaView>
-        <Text>Hi</Text>
-      </SafeAreaView>
+      <Header
+        leftComponent={{ icon: 'menu', color: 'green' }}
+        centerComponent={{ text: 'MY TITLE', style: { color: 'green' } }}
+        rightComponent={{ icon: 'home', color: 'green' }}
+      />
+      <Router />
     </NavigationContainer>
   );
 }
