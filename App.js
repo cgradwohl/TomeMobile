e024aa7132f5c0197e24ef827038326bba60cf07
Chris Gradwohl <christophergradwohl@gmail.com>
2020-06-15 20:30:14 -0700

    settings menu.

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   Text,
 } from 'react-native';
 
-import { Header } from 'react-native-elements';
-
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import {
+  Button,
+  Header,
+  Overlay,
+} from 'react-native-elements';
+
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 
@@ -30,7 +36,12 @@ const QueueScreen = () => (
 );
 
 const Router = () => (
-  <Tab.Navigator>
+  <Tab.Navigator
+    tabBarOptions={{
+      activeTintColor: 'black',
+      inactiveTintColor: 'gray',
+    }}
+  >
     <Tab.Screen name="Library" component={LibraryScreen} />
     <Tab.Screen name="Tome" component={TomeScreen} />
     <Tab.Screen name="Queue" component={QueueScreen} />
@@ -38,13 +49,57 @@ const Router = () => (
 );
 
 export default function App() {
+  const [active, setActive] = useState(false);
+
+  const SettingsMenu = () => (
+    <Overlay
+      isVisible={active}
+      onBackdropPress={() => setActive(!active)}
+      overlayStyle={{
+        width: '80%',
+        height: '75%',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+      }}
+    >
+      <>
+        <Text>Settings Menu!</Text>
+        <Button
+          buttonStyle={{
+            backgroundColor: 'black',
+            color: 'white',
+          }}
+          title="Log Out"
+        />
+      </>
+    </Overlay>
+  );
+
+  const SettingsMenuButton = () => (
+    <Button
+      buttonStyle={{
+        backgroundColor: 'black',
+      }}
+      icon={(
+        <Icon
+          color="white"
+          name="ios-menu"
+          size={20}
+        />
+      )}
+      onPress={() => setActive(!active)}
+    />
+  );
+
   return (
     <NavigationContainer>
       <Header
         backgroundColor="black"
         centerComponent={{ text: 'Tome.', style: { color: 'white' } }}
-        rightComponent={{ icon: 'menu', color: 'white' }}
+        rightComponent={<SettingsMenuButton />}
       />
+      <SettingsMenu />
       <Router />
     </NavigationContainer>
   );
